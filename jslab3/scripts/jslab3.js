ea7c3b5cfb6200cf85dfd080a9b99dcba99aa7af
Student223132 <161282535+Student223132@users.noreply.github.com>
2024-06-06 18:20:53 +0300

    jslab3_3

--- jslab3/scripts/jslab3.js
+++ jslab3/scripts/jslab3.js
@@ -3,3 +3,31 @@ export function getDecimal(n){
     return Math.round((n % 1)*10000000)/10000000;
 }
 
+export function normalizeUrl(fstr){
+    if (fstr.slice(0, 8) != 'https://'){
+        if(fstr.slice(0, 7) == 'http://'){
+            fstr.replace('http://', 'https://');
+        }
+       fstr = 'https://' + fstr;
+    }
+    if((fstr.slice(-4) != '.com')&&(fstr.slice(-4) != '.net')&&(fstr.slice(-3) != '.ru')&&(fstr.slice(-3) != '.su')){
+        fstr = fstr + '.com'
+            
+    }
+    return fstr;
+        
+}
+
+export function checkSpam(fstr){
+    let isspam = false;
+    if(fstr.search(/xxx/i)!= -1){
+        isspam = true;
+    }
+    else{
+        if(fstr.search(/viagra/i)!= -1){
+            isspam = true;        
+        }
+    }
+    return isspam;
+        
+}
