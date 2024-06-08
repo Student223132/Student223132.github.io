d590f2c7a1ea986d58e88bcf5617fd673248380b
Student223132 <161282535+Student223132@users.noreply.github.com>
2024-06-08 09:24:41 +0300

    jslab4_3

--- jslab4/scripts/main4_3.js
+++ jslab4/scripts/main4_3.js
@@ -1,14 +1,47 @@
-
+let i = 0;
+let arr = [];
+let obj1 = {}
 const button = document.getElementById('readyb');
+const button2 = document.getElementById('cleararrb');
+
 button.addEventListener('click', function() {
     let ftitle = String(title1.value);
     let fpubyear = + pubyear1.value;
     let fprice = + price1.value;
-    let book = new Book(ftitle, fpubyear, fprice);
-    book.show();
 
+    if (i<3){
+        arr.push(new Book(ftitle, fpubyear, fprice));
+        arr[i].show();
+        i = i+1;
+    }
 
+    if (i === 3){
+        for(let k = 0; k < 2; k++){
+            for(let j = 0; j< 2; j++){
+                if(arr[j].compare(arr[j+1])){
+                    obj1 = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = obj1;
+                }
+            }
+        }
+        for(let j = 0; j<3; j++){
+            arr[j].show();
+        }
+    }
+        
 });
+
+
+button2.addEventListener('click', function() {
+    arr = [];
+    i = 0;
+});
+
+if (i<3){
+    alert('Введите элемент массива')
+}
+
 
 class Book {
     constructor(btitle, bpubyear, bprice) {
@@ -17,8 +50,18 @@ class Book {
         this._price = bprice;
         this.show = function () {
             console.log("Title: ", this._title);
+            console.log("Pub year", this._pubyear);
             console.log("Price: ", this._price);
         };
+    }
+
+    compare(obj){
+        if (this._pubyear > obj._pubyear){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     get title(){
